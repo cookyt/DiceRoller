fac1b6e24cdd44f80037b0152103346d09d86584
Carlos Valera <cookyt2@gmail.com>
2013-03-31 23:18:44 -0400

    Added skeleton methods to SetSelection

--- src/com/group5/diceroller/SetSelection.java
+++ src/com/group5/diceroller/SetSelection.java
@@ -1,4 +1,40 @@
 package com.group5.diceroller;
 
+import java.util.Set;
+
 public class SetSelection {
+
+    /**
+     * Set of dice in this selection.
+     */
+    Set<DiceSet> dice_sets;
+
+    /**
+     * Randomizes the dice in this SetSelection.
+     * TODO implement
+     */
+    public void roll() {
+    }
+
+    /**
+     * Adds the given DiceSet to this selection.
+     * 
+     * @param set The DiceSet to add
+     * @return True if the set was added, false otherwise.
+     * TODO implement
+     */
+    public boolean add(DiceSet set) {
+        return false;
+    }
+
+    /**
+     * Removes the dice set with the given id from this selection.
+     * TODO implement.
+     *
+     * @param set_id The id to search for.
+     * @return True if the set was removed, false otherwise.
+     */
+    public boolean remove(int set_id) {
+        return false;
+    }
 }
