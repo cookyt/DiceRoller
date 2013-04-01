05a8b73587ad25c7da00e90429ae0cc36d59d061
Carlos Valera <cookyt2@gmail.com>
2013-03-31 23:35:50 -0400

    Added iterator to SetSelection to reduce coupling

--- src/com/group5/diceroller/SetSelection.java
+++ src/com/group5/diceroller/SetSelection.java
@@ -1,11 +1,17 @@
 package com.group5.diceroller;
 
 import java.util.Set;
+import java.util.Iterator;
 
-public class SetSelection {
+public class SetSelection
+    implements Iterable<DiceSet> {
 
     /**
      * Set of dice in this selection.
+     *
+     * NOTE to the implementor, you don't have to use a Set to implement the
+     * SetSelection. Having this here just made the class skeleton easier to
+     * write.
      */
     Set<DiceSet> dice_sets;
 
@@ -36,5 +42,16 @@ public class SetSelection {
      */
     public boolean remove(int set_id) {
         return false;
+    }
+
+    /**
+     * Returns an iterator over the DiceSets in this SetSelection.
+     * NOTE to the implementor this function is the reason the dice_sets
+     * attribute exists up above. It makes it easy to write the skeleton for
+     * this method.
+     * TODO implement
+     */
+    public Iterator<DiceSet> iterator() {
+        return dice_sets.iterator();
     }
 }
